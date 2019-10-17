3e58273f13237dcf6fc5efe11ee97b562dccdd8d
Dragos <Dragos.Dragos@honeywell.com>
2019-10-17 16:00:35 +0300

    test

--- Section_3.Course_project_the_basics/44.installing_boostrstrap_correctly/shoplist-app/src/app/app.module.ts
+++ Section_3.Course_project_the_basics/44.installing_boostrstrap_correctly/shoplist-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
